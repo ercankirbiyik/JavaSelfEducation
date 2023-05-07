59b60667742943f1d0a19d30ee5f92d56224cebc
ercankirbiyik <rcnkirbiyik@gmail.com>
2023-05-07 15:33:02 +0300

    Koşullu durumlar1 eklendi.

---
+++ src/main/java/org/example/KosulluDurumlar1.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class KosulluDurumlar1 {
+    public static void main(String[] args) {
+        /*
+
+        if (koşul) {
+            Koşul sağlanınca (true) blok çalışacak
+        }
+        else {
+                Bu bloğun üstündeki herhangi bir koşul sağlanmadığında bu blok çalışacak.
+        }
+
+         */
+
+
+        /*
+        Örnek1
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen yaşınızı giriniz: ");
+        int yas = scanner.nextInt();
+
+        if (yas < 18) {
+            System.out.println("Bu mekana giremezsiniz...");
+        }
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lütfen bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        if (sayi < 0) {
+            System.out.println("Negatif...");
+        }
+        else {
+            System.out.println("Pozitif veya sıfır...");
+        }
+
+    }
+}
