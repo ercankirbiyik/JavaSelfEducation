fa25fdfd7b526d34e38b70b02b74d8c1e38801e1
ercankirbiyik <rcnkirbiyik@gmail.com>
2023-05-07 16:12:14 +0300

    Maksimum Sayı Bulma eklendi

---
+++ src/main/java/org/example/MaksimumBulma.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MaksimumBulma {
+    public static void main(String[] args) {
+
+        //Kullanıcıdan alınan 3 sayıdan en büyük sayıyı bulma
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Birinci sayı: ");
+        int a = scanner.nextInt();
+
+        System.out.print("İkinci sayı: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Üçüncü sayı: ");
+        int c = scanner.nextInt();
+
+        int maks = -1;
+
+        if (a >= b && a >= c) {
+            maks = a;
+        } else if (b >= a && b >= c) {
+            maks = b;
+        } else {
+            maks = c;
+        }
+
+        System.out.println("Maksimum sayı : " + maks);
+
+
+    }
+
+
+}
